d7eb62715f622817f3b1ae9250afdde4c83c06c1
Momozor <momozor4@gmail.com>
2021-05-24 15:56:29 +0800

    Simplify textToMorse
    Signed-off-by: Momozor <momozor4@gmail.com>

--- tests/Utilities/functions.test.js
+++ tests/Utilities/functions.test.js
@@ -4,3 +4,8 @@ test('uuid', function(){
     expect(functions.uuid())
     .toMatch(/^[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}$/)
 })
+
+test('textToMorse', function(){
+    expect(functions.textToMorse("hello world"))
+    .toBe(".... . .-.. .-.. --- / .-- --- .-. .-.. -..")
+})
